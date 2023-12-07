cb04d4a65e61b192d596fc5e68a7c707dee01e8a
Kim <pang921008@gmail.com>
2023-12-07 17:53:04 +0800

    add input schema and type for testing

---
+++ src/main/java/com/example/training/training_project/type/Event.java
@@ -0,0 +1,12 @@
+package com.example.training.training_project.type;
+
+import lombok.Data;
+
+@Data
+public class Event {
+    private String id;
+    private String title;
+    private String description;
+    private Float price;
+    private String date;   
+}
