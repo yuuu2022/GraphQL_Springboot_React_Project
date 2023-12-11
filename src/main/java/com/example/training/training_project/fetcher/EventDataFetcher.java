ffe0e54c575ebd398ce2525bc8f0bfe2741bb67d
Kim <pang921008@gmail.com>
2023-12-11 18:00:15 +0800

    update mutation api

--- src/main/java/com/example/training/training_project/fetcher/EventDataFetcher.java
+++ src/main/java/com/example/training/training_project/fetcher/EventDataFetcher.java
@@ -1,8 +1,12 @@
 package com.example.training.training_project.fetcher;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
+import com.example.training.training_project.type.Event;
+import com.example.training.training_project.type.EventInput;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
@@ -10,14 +14,23 @@ import com.netflix.graphql.dgs.InputArgument;
 
 @DgsComponent
 public class EventDataFetcher {
-
+    private List<Event> events = new ArrayList<>();
+    
     @DgsQuery
     public List<String> events(){
         return Arrays.asList("this","is","a","list");
     }
 
     @DgsMutation
-    public String createEvenet(@InputArgument String name){
-        return name + "Created"; 
+    public Event createEvenet(@InputArgument(name ="eventInput") EventInput input){
+         Event newEvent = new Event();
+         newEvent.setId(UUID.randomUUID().toString());
+         newEvent.setTitle(input.getTitle());
+         newEvent.setDescription(input.getDescription());
+         newEvent.setPrice(input.getPrice());
+         newEvent.setDate(input.getDate());
+
+         events.add(newEvent);
+         return newEvent;
     }
 }
---
+++ src/main/java/com/example/training/training_project/type/EventInput.java
@@ -0,0 +1,11 @@
+package com.example.training.training_project.type;
+
+import lombok.Data;
+
+@Data
+public class EventInput {
+    private String title;
+    private String description;
+    private Float price;
+    private String date;   
+}
